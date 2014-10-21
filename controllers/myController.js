6526872a410b6e24fda7304feb2e65aa846fa42e
Adam Estela <adam.estela@paperg.com>
2014-10-21 00:46:15 -0700

    nav bar done

--- controllers/myController.js
+++
@@ -1,9 +0,0 @@
-function myController($scope){
-	$scope.phrase = "";
-	$scope.myOutput = "";
-
-	$scope.reversePhrase = function(){
-		$scope.phrase = $('#phraseInput').val();
-		$scope.myOutput = $scope.phrase.split('').reverse().join('');
-	};
-};
---
+++ controllers/portfolioController.js
@@ -0,0 +1,41 @@
+function portfolioController($scope){
+
+	$scope.openCategory = -1;
+
+	$scope.expandCategory = function(index){
+		// Clone the element with the auto property to see how far we should extend the height.
+		var elementToClone = $('.linkHeaderContainer').eq(index);
+		var elementCloneAuto = elementToClone.clone().css({"height":"auto"}).appendTo("body");
+		autoHeight = elementCloneAuto.css("height");
+		elementCloneAuto.remove();
+
+		// Close the previously open category
+		if($scope.openCategory > -1){
+			$( ".linkHeaderContainer" ).eq($scope.openCategory).animate({
+			    height: "20px"
+			  }, 200, function() {
+			    // Animation complete.
+			  });
+		}
+
+		// If the clicked item was already open and we just shut it, 
+		// don't open and reset $scope.openCategory.
+		if($scope.openCategory == index){
+			$scope.openCategory = -1;
+			return;
+		}
+
+		$scope.openCategory = index;
+
+		// Open the selected category.
+		$( ".linkHeaderContainer" ).eq($scope.openCategory).animate({
+		    height: autoHeight
+		  }, 200, function() {
+		    // Animation complete.
+		  });
+	};
+
+	$scope.populateGallery = function(index){
+		console.log("hello world");
+	};
+};

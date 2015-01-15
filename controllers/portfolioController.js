8a82d1d004f16294058b8f14ba6253fac4f073e8
Adam Estela <adamestela@gmail.com>
2015-01-14 20:11:59 -0800

    click to scroll tune 1

--- controllers/portfolioController.js
+++ controllers/portfolioController.js
@@ -10,7 +10,7 @@ function portfolioController($scope){
 	$scope.currentTemplate = "template";
 
 	$scope.scrollToMe = function(index){
-		$( ".screen-container" ).animate({scrollLeft: $(".template-image-restrict").eq(index).position().left}, 207 );
+		$( ".screen-container" ).animate({scrollLeft: $(".template-image-restrict").eq(index).position().left}, 828 );
 	};
 
 	self.resizeGallery = function(){
